17091957535b1c37eb7985afa234baed54d55623
MahmoudOstaz <zahoq2013@gmail.com>
2022-08-24 10:13:33 +0300

    done

---
+++ app/src/main/java/com/example/prisoners_new/salary.java
@@ -0,0 +1,14 @@
+package com.example.prisoners_new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class salary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salary);
+    }
+}
---
+++ app/src/main/java/com/example/prisoners_new/statistic.java
@@ -0,0 +1,14 @@
+package com.example.prisoners_new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class statistic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
+    }
+}
